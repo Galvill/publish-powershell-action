9046b781e5e74fbdddb93b34d6443aacb762bdef
Galvill <35693270+Galvill@users.noreply.github.com>
2023-08-04 15:45:43 +0200

    22 |  path issue

--- src/private/Copy-FilesToModuleDirectory.ps1
+++ src/private/Copy-FilesToModuleDirectory.ps1
@@ -23,9 +23,10 @@ function Copy-FilesToModuleDirectory {
     New-Item -Path $NewBasePath -Type Directory | Out-Null
     Copy-Item -Path "$ManifestDir\*" -Destination $NewBasePath -Recurse
 
-    write-host "Path: $Path"
-    write-host "NewBasePath: $NewBasePath"
+    write-host "#####Path: $Path"
+    write-host "######NewBasePath: $NewBasePath"
 
     $OutputPath = Join-Path $NewBasePath $(Split-Path -Path $Path -Leaf)
+    Write-Host "#####OutputPath: $OutputPath"
     $OutputPath
 }
